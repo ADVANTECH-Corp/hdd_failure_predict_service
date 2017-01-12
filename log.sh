74730c8b34e48ef4d81de6cd1d6b99c8eb38c8c0
ivan0124 <ivan0124.tw@gmail.com>
2017-01-12 16:19:32 +0800

    change log.sh container name

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 LOG_NAME=docker.log
-CONTAINER_NAME=ivan0124tw-hdd-failure-predict
+CONTAINER_NAME=advigw4x86/hdd-failure-predict
 #get container ID
 CONTAINER_ID=`sudo docker ps | grep $CONTAINER_NAME | awk '{ print $1}'`
 
